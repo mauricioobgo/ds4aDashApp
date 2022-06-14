dad913269bcd379552a465fb2723220b9a6dbf82
DAJASABA <dajasaba@gmail.com>
2022-06-13 19:36:36 -0500

    CHANGE

--- app/app.py
+++ app/app.py
@@ -16,7 +16,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapmi
 
 app.layout = dbc.Container([ 
 
-    dbc.Row(dbc.Col(html.H2("Your Amazing Dashboard"), width={'size': 12, 'offset': 0, 'order': 0}), style = {'textAlign': 'center', 'paddingBottom': '1%'}),
+    dbc.Row(dbc.Col(html.H2("Your Amazing Dashboard-testing"), width={'size': 12, 'offset': 0, 'order': 0}), style = {'textAlign': 'center', 'paddingBottom': '1%'}),
 
     dbc.Row(dbc.Col(dcc.Loading(children=[dcc.Graph(id ='your-graph'),
                                         dcc.Slider(id='year-slider',
