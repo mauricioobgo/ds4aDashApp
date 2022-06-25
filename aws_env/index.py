870a93d02fe42ff073901acb3f73188b8b755e86
Mauricio Obando <mauricio.obando.-nd@disney.com>
2022-06-25 13:37:17 -0500

    adding new docker container definition

--- aws_env/index.py
+++ aws_env/index.py
@@ -14,6 +14,5 @@ if __name__ == "__main__":
     app.run_server(
         host=APP_HOST,
         port=APP_PORT,
-        debug=APP_DEBUG,
-        dev_tools_props_check=DEV_TOOLS_PROPS_CHECK
+        debug=APP_DEBUG
     )
