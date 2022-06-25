94d28ef01a2c04dfdd905c55c5f6ea4980b5d845
Mauricio Obando <mauricio.obando.-nd@disney.com>
2022-06-25 10:34:02 -0500

    adding changes to structure of launch

--- environment/settings.py
+++ environment/settings.py
@@ -8,6 +8,6 @@ load_dotenv(dotenv_path=dotenv_path, override=True)
 
 APP_HOST = os.environ.get("HOST")
 #APP_PORT = int(os.environ.get("PORT"))
-APP_PORT=8085
+APP_PORT=os.environ.get("PORT")
 APP_DEBUG = bool(os.environ.get("DEBUG"))
 DEV_TOOLS_PROPS_CHECK = bool(os.environ.get("DEV_TOOLS_PROPS_CHECK"))
