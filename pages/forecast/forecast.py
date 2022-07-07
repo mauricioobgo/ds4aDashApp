e1e0c096e9b8cc6e04775a33c6073bc97d13bbd2
DAJASABA <dajasaba@gmail.com>
2022-07-07 13:22:44 -0500

    changes in Forecast, home page, add a new file (Inputs) and updated the header

--- pages/forecast/forecast.py
+++ pages/forecast/forecast.py
@@ -1,21 +1,102 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from pages.header import *
-
 from pages.forecast.forecast_data import dataframe
+from pages.forecast.inputs import *
 
 
-layout = html.Div([
-    header,
-    html.H2("FORECAST MODEL"),
-    html.Hr(),
-    dcc.Graph(id='graph-with-slider'),
-    dcc.Slider(
-        id='year-slider',
-        min=dataframe()['year'].min(),
-        max=dataframe()['year'].max(),
-        value=dataframe()['year'].min(),
-        marks={str(year): str(year) for year in dataframe()['year'].unique()},
-        step=None
-    )
-])
+###tab to predict by Location
+by_location= dbc.Card(
+    dbc.CardBody(
+        [
+            html.P("PREDICT BY LOCATION", className="card-text"),
+            dbc.Row(
+            [            
+                dbc.Col(
+                    [
+                        html.P( "The model predicts the best crop group for the selected department"),
+                        input2_1,
+                    ]
+
+                ),
+            ]
+        ),
+        ]
+    ),
+    className="mt-3",
+),
+
+###tab to select by crop group 
+by_group = dbc.Card(
+    dbc.CardBody(
+        [
+            html.P("PREDICT BY CROP GROUP", className="card-text"),
+            dbc.Row(
+            [            
+                dbc.Col(
+                    [
+                        html.P( "The model predicts the best department for the selected selected crop group"),
+                        input2_2,
+                    ]
+
+                ),
+            ]
+        ),
+        ]
+    ),
+    className="mt-3",
+),
+
+tabs = dbc.Tabs(
+    [
+        dbc.Tab(by_location, label="Tab 1"),
+        dbc.Tab(by_group, label="Tab 2"),
+    ],
+),
+
+
+layout = html.Div(
+    [
+        header,
+        html.H3("PREDICTION MODELS"),
+        html.Hr(),
+        html.P("To begin you must Select your inputs"),
+        input0,
+        input1,
+        html.Hr(),
+        tabs,
+    ]
+)
+
+
+
+
+dbc.Row(
+            [
+                
+                dbc.Col(
+                    [
+                        dcc.Graph(id='graph-with-slider'),
+                        dcc.Slider(
+                            id='year-slider',
+                            min=dataframe()['year'].min(),
+                            max=dataframe()['year'].max(),
+                            value=dataframe()['year'].min(),
+                            marks={str(year): str(year) for year in dataframe()['year'].unique()},
+                            step=None
+                        ),
+                    ]
+                ),
+                dbc.Col(
+                    [
+                        html.Thead(html.Tr([html.Th("First Name"), html.Th("Last Name")])),
+                        
+                        html.P(
+                            "Some quick example text to build on the card title and "
+                            "make up the bulk of the card's content."
+                        ),
+                    ],
+                ),
+            ],
+        ),
--- pages/home/home.py
+++ pages/home/home.py
@@ -9,20 +9,19 @@ danny = dbc.Card(
             [
                 html.H4("Danny Sanchez", className="card-title"),
                 html.P(
-                    "Some quick example text to build on the card title and "
-                    "make up the bulk of the card's content.",
+                    "Mechanical engineer with 10 years of experience in project management, visual artist and Data Science enthusiastic",
                     className="card-text",
                 ),
                 dbc.Button("sitio web", href="https://dajasaba.art/", target="_blank", color="primary"),
             ]
         ),
     ],
-    style={"width": "12rem"},
+    style={"width": "auto"},
 )
 
 guillermo = dbc.Card(
     [
-        dbc.CardImg(src="https://i.postimg.cc/yx5Qnr2j/DANNY-SANCHEZ-min.jpg", top=True),
+        dbc.CardImg(src="https://i.postimg.cc/Pr475DpJ/guillermo.png", top=True),
         dbc.CardBody(
             [
                 html.H4("Guillermo Velez", className="card-title"),
@@ -31,121 +30,118 @@ guillermo = dbc.Card(
                     "make up the bulk of the card's content.",
                     className="card-text",
                 ),
-                dbc.Button("Go somewhere", href="https://dajasaba.art/", target="_blank", color="primary"),
+                dbc.Button("Linkedin", href="https://www.linkedin.com/in/guillermo-jos%C3%A9-velez-loaiza-87833a55/", target="_blank", color="primary"),
             ]
         ),
     ],
-    style={"width": "12rem"},
+    style={"width": "auto"},
 )
 
 jaime = dbc.Card(
     [
-        dbc.CardImg(src="https://i.postimg.cc/yx5Qnr2j/DANNY-SANCHEZ-min.jpg",  top=True),
+        dbc.CardImg(src="https://i.postimg.cc/P5VFmkMk/jaime.png",  top=True),
         dbc.CardBody(
             [
                 html.H4("Jaime Delgado", className="card-title"),
                 html.P(
-                    "Some quick example text to build on the card title and "
-                    "make up the bulk of the card's content.",
+                    "Civil engineer. Master in Educational Technology Management. Experience in drawing structures in Autocad and Civil3D. Teaching experience. Currently reinforcing programming skills and learning Data Science.",
                     className="card-text",
                 ),
-                dbc.Button("sitio web", href="https://dajasaba.art/", target="_blank", color="primary"),
+                dbc.Button("Linkedin", href="https://www.linkedin.com/in/jaime--delgado", target="_blank", color="primary"),
             ]
         ),
     ],
-    style={"width": "12rem"},
+    style={"width": "auto"},
 )
 
 jehider = dbc.Card(
     [
-        dbc.CardImg(src="https://i.postimg.cc/yx5Qnr2j/DANNY-SANCHEZ-min.jpg",  top=True),
+        dbc.CardImg(src="https://i.postimg.cc/d07N6SWZ/Jehider.png",  top=True),
         dbc.CardBody(
             [
                 html.H4("Jehider   Pinto", className="card-title"),
                 html.P(
-                    "Some quick example text to build on the card title and "
-                    "make up the bulk of the card's content.",
+                    "Petroleum Engineer recently graduated from the National University of Colombia. Emphasis in the area of reservoirs and data science. ",
                     className="card-text",
                 ),
-                dbc.Button("sitio web", href="https://dajasaba.art/", target="_blank", color="primary"),
+                dbc.Button("Linkedin", href="http://www.linkedin.com/in/jehider-pinto-montes", target="_blank", color="primary"),
             ]
         ),
     ],
-    style={"width": "12rem"},
+    style={"width": "auto"},
 )
 
 joseph = dbc.Card(
     [
-        dbc.CardImg(src="https://i.postimg.cc/yx5Qnr2j/DANNY-SANCHEZ-min.jpg",  top=True),
+        dbc.CardImg(src="https://i.postimg.cc/pX78nNX3/joseph.png",  top=True),
         dbc.CardBody(
             [
                 html.H4("Joseph Martinez", className="card-title"),
                 html.P(
-                    "Some quick example text to build on the card title and "
-                    "make up the bulk of the card's content.",
+                    "Honors Industrial Engineering with knowledge in statistics; data analysis; machine learning models; Social Network Analysis and Natural Language Processing",
                     className="card-text",
                 ),
-                dbc.Button("sitio web", href="https://dajasaba.art/", target="_blank", color="primary"),
+                dbc.Button("LinkedIn", href="https://www.linkedin.com/in/joseph-martinez-/", target="_blank", color="primary"),
             ]
         ),
     ],
-    style={"width": "12rem"},
+    style={"width": "auto"},
 )
 
 mauricio = dbc.Card(
     [
-        dbc.CardImg(src="https://i.postimg.cc/yx5Qnr2j/DANNY-SANCHEZ-min.jpg",  top=True),
+        dbc.CardImg(src="https://i.postimg.cc/sggNQ27c/Mauricio.png",  top=True),
         dbc.CardBody(
             [
                 html.H4("Mauricio Obando", className="card-title"),
                 html.P(
-                    "Some quick example text to build on the card title and "
-                    "make up the bulk of the card's content.",
+                    "Mathematics student Konrad Lorenz Foundation. He works as a backend developer at Globant.",
                     className="card-text",
                 ),
-                dbc.Button("sitio web", href="https://dajasaba.art/", target="_blank", color="primary"),
+                dbc.Button("GitHub", href="https://github.com/mauricioobgo", target="_blank", color="primary"),
             ]
         ),
     ],
-    style={"width": "12rem"},
+    style={"width": "auto"},
 )
 
 nicolas = dbc.Card(
     [
-        dbc.CardImg(src="https://i.postimg.cc/yx5Qnr2j/DANNY-SANCHEZ-min.jpg",  top=True),
+        dbc.CardImg(src="https://i.postimg.cc/DfW6mc7j/nicolas.png",  top=True),
         dbc.CardBody(
             [
                 html.H4("Nicolas Cristancho", className="card-title"),
                 html.P(
-                    "Some quick example text to build on the card title and "
-                    "make up the bulk of the card's content.",
+                    "Economist and data scientist. Experience in AI modeling with satellite images and impact assessments",
                     className="card-text",
                 ),
-                dbc.Button("sitio web", href="https://dajasaba.art/", target="_blank", color="primary"),
+                dbc.Button("Linkedin", href="https://www.linkedin.com/in/nicolas-cristancho-bbba96205/", target="_blank", color="primary"),
             ]
         ),
     ],
-    style={"width": "12rem"},
+    style={"width": "auto"},
 )
 
 the_project = dbc.Card(
     dbc.CardBody(
         [
             html.H4("The Project", className="card-title"),
-            html.H6("Card subtitle", className="card-subtitle"),
+            
             html.P(
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "up the bulk of the card's content.",
+                "The proposal is based on building a model capable of predicting either which crops and/or which location will have a higher efficiency in terms of production per unit of land. ",
+                
                 className="card-text",
             ),
-            dbc.CardLink("Card link", href="#"),
-            dbc.CardLink("External link", href="https://google.com"),
-        ]
+            html.P(
+                "Agriculture industry is a key to the Colombian economy. it not only 	provides jobs to millions (about 20 percent from total) but it represents an important slice in the country GDP and almost 15 percent of total exports despite the fact that less than 5 percent of the territory is used for crops. ",
+               
+                className="card-text",
+            ),
+            html.P(
+                "Agriculture industry in Colombia is facing some challenges as climate change is affecting the agriculture industry in the entire country, affecting the efficiency in production. Volatility in their costs and prices and a strong competition with external markets gives a higher risk to investors and, Colombia investment in agriculture (11,6 percent) is below the OCDE mean value (16,8 percent)",
+                className="card-text",
+            ),
+        ],
     ),
     style={"width": "auto"},
 )
@@ -154,19 +150,12 @@ the_sources = dbc.Card(
     dbc.CardBody(
         [
             html.H4("The Sources", className="card-title"),
-            html.H6("Card subtitle", className="card-subtitle"),
+            html.H6("Municipal Agricultural Evaluations 2006-2020", className="card-subtitle"),
             html.P(
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "up the bulk of the card's content.",
+                "Municipal Agricultural Evaluations 2006-2020: information on national agricultural production. This dataset has detailed information on agricultural production, it is a panel dataset with description details of the production cycle, type of crop and yield.",
                 className="card-text",
             ),
-            dbc.CardLink("Card link", href="#"),
-            dbc.CardLink("External link", href="https://google.com"),
+            dbc.CardLink("Municipal Agricultural Evaluations", href="https://www.datos.gov.co/Agricultura-y-Desarrollo-Rural/Evaluaciones-Agropecuarias-Municipales-EVA/2pnw-mmge/data"),
         ]
     ),
     style={"width": "auto"},
@@ -176,19 +165,6 @@ the_team = dbc.Card(
     dbc.CardBody(
         [
             html.H4("The Team", className="card-title"),
-            html.H6("Card subtitle", className="card-subtitle"),
-            html.P(
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "Some quick example text to build on the card title and make "
-                "up the bulk of the card's content.",
-                className="card-text",
-            ),
-            dbc.CardLink("Card link", href="#"),
-            dbc.CardLink("External link", href="https://google.com"),
         ]
     ),
     style={"width": "auto"},
@@ -205,17 +181,17 @@ layout = dbc.Container(
         dbc.Row(dbc.Col(the_team, width="auto"),className="mt-4"),       
         dbc.Row(
             [
-                dbc.Col(danny, width="auto"),
-                dbc.Col(guillermo, width="auto"),
-                dbc.Col(jaime, width="auto"),
-                dbc.Col(jehider, width="auto"),              
+                dbc.Col(danny, width=3),
+                dbc.Col(guillermo, width=3),
+                dbc.Col(jaime, width=3),
+                dbc.Col(jehider, width=3),              
             ],className="mt-4"
         ),
         dbc.Row(
             [
-                dbc.Col(joseph, width="auto"),
-                dbc.Col(mauricio, width="auto"),
-                dbc.Col(nicolas, width="auto"),
+                dbc.Col(joseph, width=3),
+                dbc.Col(mauricio, width=3),
+                dbc.Col(nicolas, width=3),
             ],className="mt-4"
         ),
         
